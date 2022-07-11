81f15c1bf76ac1791e20d5f9987d5ba60c881fb2
Alex <alex.santox@hotmail.com>
2022-07-10 21:04:01 -0300

    Atualizar

---
+++ espec.js
@@ -0,0 +1,26 @@
+  function abrirAlerta1(){
+    swal({
+  title: "Software compatíveis",
+  text: "Adobe Photoshop \n Bizagi Modeler\n Citrix\n Delphi Embarcadeiro \n Doors \n GBI \n GBI 3D \n SPM + SEE \n Tortoise \n Visual Fortran Compiler \n Microsoft Teams \n SAP GUI \n Pacote Office \n Microsoft Outlook \n \n ➡️ Para saber sobre softwares que não estão na lista, favor mandar e-mail nosso suporte: \n\n SUPORTE.PC@EMBRAER.COM.BR",
+  icon: 'info',
+  button: "Voltar",
+});
+  }
+
+  function abrirAlerta2(){
+    swal({
+  title: "Software compatíveis",
+  text: "Adobe Photoshop\nBDM\nBizagi Modeler\nCitrix\nDelphi Embarcadeiro\nDoors\nDPE\nGBI\nGBI 3D\nMATLAB\nMES / MES 3D / PIM 3D\nSPM + SEE\nTortoise\nVisual Fortran Compiler\nMicrosoft Teams\nSAP GUI\nPacote Office\nMicrosoft Excel Desenvolvimento\nMicrosoft Outlook\n \n ➡️ Para saber sobre softwares que não estão na lista, favor mandar e-mail nosso suporte: \n\n SUPORTE.PC@EMBRAER.COM.BR",
+  icon: 'info',
+  button: "Voltar",
+});
+  }
+
+  function abrirAlerta3(){
+    swal({
+  title: "Software compatíveis",
+  text: "Adobe Photoshop\nBDM\nBizagi Modeler\nCitrix\nCreoView\nDelphi Embarcadeiro\nDoors\nDPE\nGBI\nGBI 3D\nMATLAB\nMES / MES 3D / PIM 3D\nSPM + SEE\nTortoise\nVisual Fortran Compiler\nMicrosoft Teams\nSAP GUI\nPacote Office\nMicrosoft Excel Desenvolvimento\nMicrosoft Outlook\nIGE + XAO\nDMU Navigator\n\n➡️ Para saber sobre softwares que não estão na lista, favor mandar e-mail nosso suporte: \n\n SUPORTE.PC@EMBRAER.COM.BR",
+  icon: 'info',
+  button: "Voltar",
+});
+  }
